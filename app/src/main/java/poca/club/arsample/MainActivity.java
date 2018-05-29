b07dd874ebf7900b9951df78adc176e2485ba511
ar9013 <ar9013@gmail.com>
2018-05-29 14:07:47 +0800

    add LabActivity

--- app/src/main/java/poca/club/arsample/MainActivity.java
+++ app/src/main/java/poca/club/arsample/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG ="MainActivity";
     private static final int CAMERA =5;
+    private static final int WRITE_EXTERNAL_STORAGE =3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +22,31 @@ public class MainActivity extends AppCompatActivity {
 
         // 檢查相機權限
         Ask.on(MainActivity.this).id(CAMERA).forPermissions(Manifest.permission.CAMERA).go();
-
+        Ask.on(MainActivity.this).id(WRITE_EXTERNAL_STORAGE).forPermissions(Manifest.permission.WRITE_EXTERNAL_STORAGE).go();
 
 
     }
 
     @AskDenied(Manifest.permission.CAMERA)
-    public void CamerasDenied(int id){
+    public void Cameras_Denied(int id){
         Log.d(TAG,"CAMERA DENiED");
     }
 
     @AskGranted(Manifest.permission.CAMERA)
-    public void CamerasGranted(int id){
+    public void Cameras_Granted(int id){
         Log.d(TAG,"CAMERA GRANTED");
     }
+
+    @AskDenied(Manifest.permission.CAMERA)
+    public void WRITE_EXTERNAL_STORAGE_Denied(int id){
+        Log.d(TAG,"CAMERA DENiED");
+    }
+
+    @AskGranted(Manifest.permission.CAMERA)
+    public void WRITE_EXTERNAL_STORAGE_Granted(int id){
+        Log.d(TAG,"CAMERA GRANTED");
+    }
+
+
 
 }
